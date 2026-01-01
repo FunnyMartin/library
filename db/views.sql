5e5818360812893e3eb5cbc58458861e4c4b251d
FunnyMartin <silar.martin@post.cz>
2026-01-01 18:47:45 +0100

    Add database views

---
+++ db/views.sql
@@ -0,0 +1,16 @@
+create view vw_not_returned 
+as
+select m.name, m.surname, b.title, c.status, l.due_date from loans l 
+join members m on m.id = l.member_id
+join copies c on c.id = l.copy_id
+join books b on b.id = c.book_id
+where l.return_date is null;
+    
+
+create view vw_book_stat 
+as 
+select b.title, count(l.id) as amount_of_loans from loans l
+join copies c on c.id = l.copy_id
+join books b on b.id = c.book_id
+group by b.title
+order by amount_of_loans asc;
